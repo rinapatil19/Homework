6c6e76fae4cbae3858cc288a326d824c4fc200c6
rinapatil19 <160730703+rinapatil19@users.noreply.github.com>
2026-02-11 13:34:43 +0530

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,21 @@
+#1=>Home Work
+# Accept decimal number 
+# num = int(input("Enter decimal number:"))
+
+#convert
+# binary = bin(num)
+# octal = oct(num)
+# hexa = hex(num)
+
+# print("Binary=", binary)
+# print("octal=", octal)
+# print("Hexadecimal", hexa)
+
+#2=>Home Work
+# Accept number from user
+num = input("Enter binary/octal/hexadecimal number (with 0b, 0o, 0x): ")
+
+# convert to decimal
+decimal = int(num, 0)
+
+print("Decimal=", decimal)
